ed7126f00a47991bf5e1ffe6435c81312d32eb2e
xeniawetzel <92883462+xeniawetzel@users.noreply.github.com>
2023-11-24 13:56:50 +0100

    working on finishing `interpret`

--- flummi/interpreter/interpreter.py
+++ flummi/interpreter/interpreter.py
@@ -14,6 +14,10 @@ __all__ = (
     "interpret",
 )
 
+
+class LoopBreak(Exception): ...
+
+
 def interpret(program: proc.Program[E, T]) -> list[duckdb.DuckDBPyRelation]:
     """
     Interprets given program
@@ -76,17 +80,33 @@ def statement_helper(statement: proc.Statement[E, T], env: dict[common.Variable,
 
     match statement:
         case proc.Loop(name, body):
-            ...
+            loop_variable = common.Variable(name)
+            env[loop_variable] = body
+            while(env[loop_variable] != "BREAK"):
+                try:
+                    env, return_list = statement_helper(body, env, return_list)
+                except LoopBreak as e:
+                    if name == e.args[0]:
+                        break
+                    else:
+                        raise e
+            return env, return_list
+        
         case proc.Continue(name):
-            ...
+            return env, return_list
+        
         case proc.Break(name):
-            ...
+            raise LoopBreak(name)
+            break_variable = list({k:v for k,v in env.items() if k.identifier == name}.keys())[0]
+            env[break_variable] = "BREAK"
+            return env, return_list
+        
         case proc.If(condition, t_branch, f_branch):
             ...
         case proc.Emit(to_emit):
             values = []
             for variable in to_emit.free_variables:
-                value = env[variable]
+                value = replace_values(variable, env[variable], env)
                 values.append(value)
             
             formatted_emit = to_emit.source.format(*values)
@@ -98,8 +118,7 @@ def statement_helper(statement: proc.Statement[E, T], env: dict[common.Variable,
             return env, return_list
 
         case proc.Assignment(variable, expression):
-            formatted_exp = expression.source.format(expression.free_variables)
-            env[variable] = [*duckdb.execute("SELECT " + formatted_exp).fetchnumpy().values()][0][0]
+            env[variable] = expression
             return env, return_list
 
         case proc.Block(statements):
@@ -109,6 +128,13 @@ def statement_helper(statement: proc.Statement[E, T], env: dict[common.Variable,
             
         case proc.Stop():
             return env, return_list
+        
 
-        case _:
-            print("nothing to see here yet")
+def replace_values(variable: common.Variable, expression: common.Expression[E], env: dict[common.Variable, any]) -> common.Expression[E]:
+    formatted_exp = f"{expression.source}".format(*expression.free_variables)
+    if(expression.free_variables == 0):
+        env[variable] = [*duckdb.execute("SELECT " + formatted_exp).fetchnumpy().values()][0][0]
+        return env[variable]
+    formatted_exp = f"{expression.source}".format(*list(map(lambda x: replace_values(x, env[x], env), expression.free_variables)))
+    env[variable] = formatted_exp
+    return env[variable]
