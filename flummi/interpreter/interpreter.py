0a4172ab2a7b17fb9b749e2c03a227b119cab43a
xeniawetzel <92883462+xeniawetzel@users.noreply.github.com>
2023-11-29 11:01:14 +0100

    implemented `continue` and `stop`. Addd documentation for the methods and addd a few comments. Completed implementation of the interpreter.

--- flummi/interpreter/interpreter.py
+++ flummi/interpreter/interpreter.py
@@ -11,11 +11,13 @@ E = TypeVar("E", bound=common.SupportsFormat)
 T = TypeVar("T", bound=common.SupportsStr)
 
 __all__ = (
-    "interpret",
+    "Interpreter",
 )
 
 
 class LoopBreak(Exception): ...
+class LoopContinue(Exception): ...
+class Stop(Exception): ...
 
 class Interpreter():
     return_list: list[duckdb.DuckDBPyRelation] = []
@@ -26,17 +28,18 @@ class Interpreter():
         """
         Interprets given program
 
-        Parameters:
+        Parameters: program (proc.Program[E, T]): program to be interpreted
 
-        Returns:
+        Returns: None
 
         """
 
-        #get return type and first statement of function from program
         first_function_statement = self.program_helper(program)
 
-        #go through statements
-        self.statement_helper(first_function_statement)
+        try: 
+            self.statement_helper(first_function_statement)
+        except Stop:
+            return self.return_list
 
         return self.return_list
 
@@ -44,20 +47,16 @@ class Interpreter():
         """
         Assigns program arguments to function parameters and appends them at the front of the statement list of the function
 
-        Parameters:
+        Parameters: program (proc.Program[E, T]): program to be interpreted
 
-        Returns:
+        Returns: None
         """
 
-        # for faster access
         f = program.function
         
         if len(f.parameters) != len(program.inputs):  
             raise TypeError("Incorrect number of arguments given.")
 
-        #create assignments for parameters <- arguments
-        #add them to a block
-        #add the block at the beginning of the function body
         temp_block = proc.Block([]) 
 
         for x in range(0, len(f.parameters)):
@@ -67,19 +66,20 @@ class Interpreter():
             temp_declaration = proc.Declaration(parameter_keys[x], parameter_values[x])
             temp_block.statements.append(temp_declaration)
             temp_block.statements.append(temp_assignment)
-        
+
         temp_block.statements.append(f.body)
+        
         f.body = temp_block
 
         return f.body
 
-    def statement_helper(self, statement: proc.Statement[E, T]) -> dict[common.Variable, any]:
+    def statement_helper(self, statement: proc.Statement[E, T]) -> None:
         """
         Goes through all statements of a function to return result(s)
 
-        Parameters:
+        Parameters: statement (proc.Statement[E, T]): statement to be matched
 
-        Returns:
+        Returns: None
         
         """
         match statement:
@@ -92,10 +92,14 @@ class Interpreter():
                             break
                         else:
                             raise e
-                ...
+                    except LoopContinue as e:
+                        if name == e.args[0]:
+                            continue
+                        else:
+                            raise e
             
             case proc.Continue(name):
-                ...
+                raise LoopContinue(name)
             
             case proc.Break(name):
                 raise LoopBreak(name)
@@ -109,7 +113,6 @@ class Interpreter():
             case proc.Emit(to_emit):
                 result = self.expression_helper(to_emit)
                 self.return_list.append(result)
-                ...
 
             case proc.Declaration(variable, type):
                 self.types[variable] = re.match(r"(\w+)",type.source).group(0)
@@ -120,20 +123,37 @@ class Interpreter():
             case proc.Block(statements):
                 for block_statement in statements:
                     self.statement_helper(block_statement)
-                
+
             case proc.Stop():
-                ...
+                raise Stop
+                
             
 
     def expression_helper(self, expression: common.Expression[E]) -> str:
+        """
+        Inserts values into placeholders, runs it as a DuckDB query and returns the result
+
+        Parameters: expression (common.Expression[E]): expression to be queried
+
+        Returns: str: Value of the expression in as a str
+        
+        """
+
+        # makes a list with all unique placeholer numbers in the expression
         placeholder_list = list(dict.fromkeys(re.findall(r"\{(\d)\}", expression.source)))
 
+        # if free veriables exist in the expression 
         if len(expression.free_variables) != 0:
+            # then get its type and its corresponding placement number 
             placeholder_tuple = [(self.types[expression.free_variables[int(x)]], placeholder_list[int(x)]) for x in placeholder_list]
+            # and make a new String where each placeholder number is incremented by one (for sql) and bring it in the form of ({x} :: type)
             placeholder_list = list(map(lambda tuple: f"({{{str(int(tuple[1])+1)}}} :: {tuple[0]})", placeholder_tuple))
 
+        # use these new strings and format the expression
         temp_expression = expression.source.format(*placeholder_list)
+        # substitute all python placeholders with sql placeholders
         temp_expression = re.sub(r"\{(\d)\}", r"$\1", temp_expression)
         
+        #run the sql query and return it
         return duckdb.execute(f"SELECT ({temp_expression})", [self.env[x] for x in expression.free_variables]).fetchone()[0]
 
