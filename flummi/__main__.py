ce7bd9f9c5e42fe818103ebee8d8ab9e8beb64d9
Tim Fischer <tim.fischer@uni-tuebingen.de>
2024-02-07 08:44:34 +0100

    Add loopless rule

--- flummi/__main__.py
+++ flummi/__main__.py
@@ -34,124 +34,132 @@ class Flag(Enum):
     AVOID_MULTIPLE_RECURSIVE_REFERENCE = auto()
     INJECT_TRACE_GENERATION = auto()
     INCLUDE_EMIT_ORDER = auto()
+    FORCE_WITH_RECURSIVE = auto()
 
 
 def main():
     parser = argparse.ArgumentParser("flummi", description="CTE-focussed compilation of imperative programs to recursive SQL.")
-    parser.add_argument('infile', type=argparse.FileType('r'))
-    parser.add_argument('-o', '--output', type=argparse.FileType('w'), default="out.sql", help="The file to write the compilation result to.")
-    parser.add_argument('-v', '--verbose', default=0, action="count", help="Control the level of verbosity.")
-    parser.add_argument('-g', '--graphs', default=None, type=Path, help="Directory to write graphviz files for each transformation to.")
-    parser.add_argument('-i', '--intermediates', default=None, type=Path, help="Directory to write IR representation for each transformation to.")
-    parser.add_argument('-f', '--flag', action='append', choices=Flag._member_map_.keys(), help="Configure compilation.")
-    parser.add_argument('-d', '--dbms', choices=['duckdb','postgres','umbra'], help="Apply DBMS specific flag set.")
+    subparsers = parser.add_subparsers(dest='command')
+
+    compiler_parser = subparsers.add_parser('compile')
+    compiler_parser.add_argument('infile', type=argparse.FileType('r'))
+    compiler_parser.add_argument('-o', '--output', type=argparse.FileType('w'), default="out.sql", help="The file to write the compilation result to.")
+    compiler_parser.add_argument('-v', '--verbose', default=0, action="count", help="Control the level of verbosity.")
+    compiler_parser.add_argument('-g', '--graphs', default=None, type=Path, help="Directory to write graphviz files for each transformation to.")
+    compiler_parser.add_argument('-i', '--intermediates', default=None, type=Path, help="Directory to write IR representation for each transformation to.")
+    compiler_parser.add_argument('-f', '--flag', action='append', choices=Flag._member_map_.keys(), help="Configure compilation.")
+    compiler_parser.add_argument('-d', '--dbms', choices=['duckdb','postgres','umbra'], help="Apply DBMS specific flag set.")
+
     arguments = parser.parse_args()
 
-    flags = {
-        Flag[flag_name]
-        for flag_name in arguments.flag or []
-    }
+    match arguments.command:
+        case "compile":
+            flags = {
+                Flag[flag_name]
+                for flag_name in arguments.flag or []
+            }
 
-    match arguments.dbms:
-        case 'postgres':
-            flags.update({
-                Flag.AVOID_MULTIPLE_RECURSIVE_REFERENCE
-            })
-        case 'duckdb':
-            flags.update({
-                Flag.EXPLICIT_MATERIALIZED
-            })
-        case 'umbra':
-            flags.update({
+            match arguments.dbms:
+                case 'postgres':
+                    flags.update({
+                        Flag.AVOID_MULTIPLE_RECURSIVE_REFERENCE
+                    })
+                case 'duckdb':
+                    flags.update({
+                        Flag.EXPLICIT_MATERIALIZED
+                    })
+                case 'umbra':
+                    flags.update({
 
-            })
+                    })
 
-    if arguments.graphs and not arguments.graphs.exists():
-        arguments.graphs.mkdir()
+            if arguments.graphs and not arguments.graphs.exists():
+                arguments.graphs.mkdir()
 
-    if arguments.intermediates and not arguments.intermediates.exists():
-        arguments.intermediates.mkdir()
+            if arguments.intermediates and not arguments.intermediates.exists():
+                arguments.intermediates.mkdir()
 
-    printer = Printer(arguments.verbose)
+            printer = Printer(arguments.verbose)
 
-    def print_stats(stats):
-        printer[1](f"\033[1;2m>> \033[0m{stats!r}")
+            def print_stats(stats):
+                printer[1](f"\033[1;2m>> \033[0m{stats!r}")
 
-    def print_graph(graph: CFG.Graph, path: str):
-        if arguments.graphs:
-            with open(arguments.graphs / path, "w+") as f:
-                f.write(dot(graph))
-            printer[1](f"\033[1;2m>> \033[0;2;4m{arguments.graphs / path}\033[0m")
+            def print_graph(graph: CFG.Graph, path: str):
+                if arguments.graphs:
+                    with open(arguments.graphs / path, "w+") as f:
+                        f.write(dot(graph))
+                    printer[1](f"\033[1;2m>> \033[0;2;4m{arguments.graphs / path}\033[0m")
 
-    def print_intermediate(graph: CFG.Graph, path: str):
-        if arguments.intermediates:
-            with open(arguments.intermediates / path, "w+") as f:
-                STYLE.off()
-                f.write(pretty(graph))
-                STYLE.on()
-            printer[1](f"\033[1;2m>> \033[0;2;4m{arguments.intermediates / path}\033[0m")
+            def print_intermediate(graph: CFG.Graph, path: str):
+                if arguments.intermediates:
+                    with open(arguments.intermediates / path, "w+") as f:
+                        STYLE.off()
+                        f.write(pretty(graph))
+                        STYLE.on()
+                    printer[1](f"\033[1;2m>> \033[0;2;4m{arguments.intermediates / path}\033[0m")
 
-    source = arguments.infile.read()
+            source = arguments.infile.read()
 
-    ast = parse(source)
+            ast = parse(source)
 
-    ast, symbol_table, emit_type, variable_bindings = analyze(ast)
+            ast, symbol_table, emit_type, variable_bindings = analyze(ast)
 
-    printer[1]("\033[1;2m[0]\033[0;36m lowering to CFG\033[0m")
-    cfg = lower(ast)
-    print_graph(cfg, "0_lowering.gv")
-    print_intermediate(cfg, "0_lowering.flir")
-    printer[2](pretty(cfg))
+            printer[1]("\033[1;2m[0]\033[0;36m lowering to CFG\033[0m")
+            cfg = lower(ast)
+            print_graph(cfg, "0_lowering.gv")
+            print_intermediate(cfg, "0_lowering.flir")
+            printer[2](pretty(cfg))
 
-    printer[1]("\033[1;2m[1]\033[0;36m optimizing CFG\033[0m")
-    cfg, optimizer_statistics = optimize(cfg)
-    print_stats(optimizer_statistics)
-    print_graph(cfg, "1_optimize.gv")
-    print_intermediate(cfg, "1_optimize.flir")
-    printer[2](pretty(cfg))
+            printer[1]("\033[1;2m[1]\033[0;36m optimizing CFG\033[0m")
+            cfg, optimizer_statistics = optimize(cfg)
+            print_stats(optimizer_statistics)
+            print_graph(cfg, "1_optimize.gv")
+            print_intermediate(cfg, "1_optimize.flir")
+            printer[2](pretty(cfg))
 
-    printer[1](f"\033[1;2m[2]\033[0;36m placing additional JUMPs\033[0m")
-    if Flag.JUMPS_ONLY in flags:
-        for block in cfg.blocks.values():
-            for label in cfg.blocks.keys():
-                block.terminal = CFG.jumpify(block.terminal, label)
-    elif Flag.JUMP_INTO_TRACES in flags:
-        for trace_head, *_ in find_traces(cfg):
-            for block in cfg.blocks.values():
-                block.terminal = CFG.jumpify(block.terminal, trace_head)
-    elif Flag.JUMP_INTO_LOOPS in flags:
-        successors = label_graph.collect_successors(cfg)
-        loop_heads = label_graph.loop_heads(successors)
-        for block in cfg.blocks.values():
-            for loop_head in loop_heads:
-                block.terminal = CFG.jumpify(block.terminal, loop_head)
+            printer[1](f"\033[1;2m[2]\033[0;36m placing additional JUMPs\033[0m")
+            if Flag.JUMPS_ONLY in flags:
+                for block in cfg.blocks.values():
+                    for label in cfg.blocks.keys():
+                        block.terminal = CFG.jumpify(block.terminal, label)
+            elif Flag.JUMP_INTO_TRACES in flags:
+                for trace_head, *_ in find_traces(cfg):
+                    for block in cfg.blocks.values():
+                        block.terminal = CFG.jumpify(block.terminal, trace_head)
+            elif Flag.JUMP_INTO_LOOPS in flags:
+                successors = label_graph.collect_successors(cfg)
+                loop_heads = label_graph.loop_heads(successors)
+                for block in cfg.blocks.values():
+                    for loop_head in loop_heads:
+                        block.terminal = CFG.jumpify(block.terminal, loop_head)
 
-    print_graph(cfg, "2_additional_jumps.gv")
-    print_intermediate(cfg, "2_additional_jumps.flir")
-    printer[2](pretty(cfg))
+            print_graph(cfg, "2_additional_jumps.gv")
+            print_intermediate(cfg, "2_additional_jumps.flir")
+            printer[2](pretty(cfg))
 
-    printer[1]("\033[1;2m[3]\033[0;36m materializing data flow\033[0m")
-    cfg, data_flow_statistics = materialize_data_flow(cfg)
-    print_stats(data_flow_statistics)
-    print_graph(cfg, "3_materialize_data_flow.gv")
-    print_intermediate(cfg, "3_materialize_data_flow.flir")
-    printer[2](pretty(cfg))
+            printer[1]("\033[1;2m[3]\033[0;36m materializing data flow\033[0m")
+            cfg, data_flow_statistics = materialize_data_flow(cfg)
+            print_stats(data_flow_statistics)
+            print_graph(cfg, "3_materialize_data_flow.gv")
+            print_intermediate(cfg, "3_materialize_data_flow.flir")
+            printer[2](pretty(cfg))
 
-    printer[1]("\033[1;2m[4]\033[0;36m generating SQL code\033[0m")
-    sql = codegen(
-        cfg,
-        symbol_table,
-        emit_type,
-        variable_bindings,
-        include_trace=Flag.INJECT_TRACE_GENERATION in flags,
-        explicit_materialized=Flag.EXPLICIT_MATERIALIZED in flags,
-        avoid_multiple_recursive_references=Flag.AVOID_MULTIPLE_RECURSIVE_REFERENCE in flags,
-        include_emit_order=Flag.INCLUDE_EMIT_ORDER in flags,
-    )
+            printer[1]("\033[1;2m[4]\033[0;36m generating SQL code\033[0m")
+            sql = codegen(
+                cfg,
+                symbol_table,
+                emit_type,
+                variable_bindings,
+                include_trace=Flag.INJECT_TRACE_GENERATION in flags,
+                explicit_materialized=Flag.EXPLICIT_MATERIALIZED in flags,
+                avoid_multiple_recursive_references=Flag.AVOID_MULTIPLE_RECURSIVE_REFERENCE in flags,
+                include_emit_order=Flag.INCLUDE_EMIT_ORDER in flags,
+                force_with_recursive=Flag.FORCE_WITH_RECURSIVE in flags,
+            )
 
-    if arguments.output:
-        printer[1](f"\033[1;2m*\033[0;36m writing output to \033[4m'{arguments.output.name}'\033[0m")
-        arguments.output.write(sql + "\n")
+            if arguments.output:
+                printer[1](f"\033[1;2m*\033[0;36m writing output to \033[4m'{arguments.output.name}'\033[0m")
+                arguments.output.write(sql + "\n")
 
 
 if __name__ == "__main__":
--- flummi/codegen.py
+++ flummi/codegen.py
@@ -31,7 +31,8 @@ def codegen(
     include_trace: bool = False,
     explicit_materialized: bool = False,
     avoid_multiple_recursive_references: bool = False,
-    include_emit_order: bool = False
+    include_emit_order: bool = False,
+    force_with_recursive: bool = False,
 ) -> str:
     return CodeGen(
         symbol_table,
@@ -40,7 +41,8 @@ def codegen(
         include_trace,
         explicit_materialized,
         avoid_multiple_recursive_references,
-        include_emit_order
+        include_emit_order,
+        force_with_recursive,
     ).gen_program(graph)
 
 
@@ -57,11 +59,13 @@ class CodeGen:
     explicit_materialized: bool
     avoid_multiple_recursive_references: bool
     include_emit_order: bool
+    force_with_recursive: bool
 
     entry_label: CFG.BlockLabel = field(init=False)
     inputs: dict[CFG.BlockLabel, list[grammar.Variable]] = field(init=False)
     jump_predecessors: LabelGraph = field(init=False)
     goto_predecessors: LabelGraph = field(init=False)
+    jump_variables: list[grammar.Variable] = field(init=False)
     emit_type_sql: str = field(init=False)
     condition_variable_counter: int = field(init=False, default=0)
 
@@ -89,13 +93,136 @@ class CodeGen:
         self.jump_predecessors = invert_label_graph(collect_jumps(graph))
         self.goto_predecessors = invert_label_graph(collect_gotos(graph))
         unsorted_inputs, unsorted_jump_variables = get_block_inputs(graph)
-        jump_variables = list(sorted(unsorted_jump_variables, key=lambda variable: variable.identifier))
+        self.jump_variables = list(sorted(unsorted_jump_variables, key=lambda variable: variable.identifier))
         self.inputs = {
             label: list(sorted(inputs, key=lambda variable: variable.identifier))
             for label, inputs in unsorted_inputs.items()
         }
 
+        if self.force_with_recursive or any(pred for pred in self.jump_predecessors.values()):
+            return self.gen_program_with_jumps(graph)
+        else:
+            return self.gen_program_without_jumps(graph)
 
+    def gen_program_without_jumps(self, graph: CFG.Graph) -> str:
+        emit_sources = [
+            label
+            for label, block in graph.blocks.items()
+            if CFG.contains_emits(block)
+        ]
+
+        working_table_columns_sql = (
+            ', ' * (0 < len(self.jump_variables)) +
+            ', '.join(
+                self.gen_variable(variable)
+                for variable in self.jump_variables
+            )
+        )
+
+        working_table_nulls_sql = (
+            ', ' * (0 < len(self.jump_variables)) +
+            ', '.join(
+                f"CAST(NULL AS {self.gen_type(self.symbol_table[variable])})"
+                for variable in self.jump_variables
+            )
+        )
+
+        initial_row_sql = _indent(
+            ',\n'.join(
+                f"CAST({self.gen_expression(self.variable_bindings[variable]) if variable in self.variable_bindings else "NULL"} AS {self.gen_type(self.symbol_table[variable])}) AS \"{variable.identifier}\""
+                for variable in self.jump_variables
+            ),
+            ' ' * 19
+        )
+
+        blocks = _indent(',\n'.join(
+            self.gen_block(graph.blocks[label])
+            for label in dependent_ordering(collect_gotos(graph))
+        ), ' ' * 11)
+
+        trace_column_sql = (
+            ', "%trace%"'
+            if self.include_trace else
+            ""
+        )
+
+        trace_null_column_sql = (
+            'CAST(NULL AS json) AS "%trace%"'
+            if self.include_trace else
+            ""
+        )
+
+        step_column_sql = (
+            ', "%step%"'
+            if self.include_trace else
+            ""
+        )
+
+        step_zero_column_sql = (
+            'CAST(0 AS int) AS "%step%"'
+            if self.include_trace else
+            ""
+        )
+
+        step_null_column_sql = (
+            'CAST(NULL AS int) AS "%step%"'
+            if self.include_trace else
+            ""
+        )
+
+        ordinaltiy_column_sql = (
+            ', "%ordinality%"'
+            if self.include_emit_order else
+            ""
+        )
+
+        ordinaltiy_zero_column_sql = (
+            'CAST(0 AS int) AS "%ordinality%"'
+            if self.include_emit_order else
+            ""
+        )
+
+        return dedent(f"""
+        WITH
+          "%loop%"("%kind%", "%label%"{working_table_columns_sql}, "%result%"{trace_column_sql}{step_column_sql}{ordinaltiy_column_sql}) AS (
+            SELECT 'jump' AS "%kind%",
+                   '{graph.entry_label.label}' AS "%label%",
+                   {initial_row_sql}{(',\n' + ' ' * 19) * bool(initial_row_sql)}CAST(NULL AS {self.emit_type_sql}) AS "%result%"{(',\n' + ' ' * 19) * (self.include_trace or self.include_emit_order)}{trace_null_column_sql}{(',\n' + ' ' * 20) * self.include_trace}{step_zero_column_sql}{', ' * (self.include_trace and self.include_emit_order)}{ordinaltiy_zero_column_sql}
+           ),{blocks}
+
+        {
+           _indent(
+               '\n  UNION ALL\n'.join(
+                   chain(
+                       (
+                           dedent(
+                               f"""
+                               SELECT "%result%"{', ' * self.include_trace}{trace_null_column_sql}{', ' * self.include_trace}{step_null_column_sql}{ordinaltiy_column_sql}
+                               FROM   "{label.label}"
+                               WHERE  "%kind%"='emit'
+                               """
+                           )[1:-1]
+                           for label in emit_sources
+                       ),
+                       (
+                           dedent(
+                               f"""
+                               SELECT CAST(NULL AS {self.emit_type_sql}){trace_column_sql}{step_column_sql}{ordinaltiy_column_sql}
+                               FROM   "{label.label}"
+                               WHERE  "%kind%"='trace'
+                               """
+                           )[1:-1]
+                           for label in graph.blocks
+                           if self.include_trace
+                       )
+                   )
+               ),
+               ' ' * 8
+           )
+        };
+        """)[1:-1]
+
+    def gen_program_with_jumps(self, graph: CFG.Graph) -> str:
         jump_sources = [
             label
             for label, block in graph.blocks.items()
@@ -109,25 +236,25 @@ class CodeGen:
         ]
 
         working_table_columns_sql = (
-            ', ' * (0 < len(jump_variables)) +
+            ', ' * (0 < len(self.jump_variables)) +
             ', '.join(
                 self.gen_variable(variable)
-                for variable in jump_variables
+                for variable in self.jump_variables
             )
         )
 
         working_table_nulls_sql = (
-            ', ' * (0 < len(jump_variables)) +
+            ', ' * (0 < len(self.jump_variables)) +
             ', '.join(
                 f"CAST(NULL AS {self.gen_type(self.symbol_table[variable])})"
-                for variable in jump_variables
+                for variable in self.jump_variables
             )
         )
 
         initial_row_sql = _indent(
             ',\n'.join(
                 f"CAST({self.gen_expression(self.variable_bindings[variable]) if variable in self.variable_bindings else "NULL"} AS {self.gen_type(self.symbol_table[variable])}) AS \"{variable.identifier}\""
-                for variable in jump_variables
+                for variable in self.jump_variables
             ),
             ' ' * 20
         )
