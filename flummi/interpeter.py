9ee9601cfd4c6884126c892980513ba97bd32a12
Tim Fischer <tim.fischer@uni-tuebingen.de>
2024-02-14 09:38:50 +0100

    Simplified SQL expression preparation in interpreter

--- flummi/interpeter.py
+++ flummi/interpeter.py
@@ -114,24 +114,18 @@ class Interpreter:
         Returns: str: Value of the expression in as a str
 
         """
+        result = duckdb.execute(
+            "SELECT " + expression.source.format(*(
+                f"(${i+1} :: {self.symbol_table[free_variable].source})"
+                for i, free_variable in enumerate(expression.free_variables)
+            )),
+            [
+                self.environment[free_variable]
+                for free_variable in expression.free_variables
+            ]
+        )
 
-        # makes a list with all unique placeholder numbers in the expression
-        placeholder_list = list(dict.fromkeys(re.findall(r"\{(\d)\}", expression.source)))
-
-        # if free variables exist in the expression
-        if len(expression.free_variables) != 0:
-            # then get its type and its corresponding placement number
-            placeholder_tuple = [(self.symbol_table[expression.free_variables[int(x)]].source, placeholder_list[int(x)]) for x in placeholder_list]
-            # and make a new String where each placeholder number is incremented by one (for sql) and bring it in the form of ({x} :: type)
-            placeholder_list = list(map(lambda tuple: f"({{{str(int(tuple[1])+1)}}} :: {tuple[0]})", placeholder_tuple))
-
-        # use these new strings and format the expression
-        temp_expression = expression.source.format(*placeholder_list)
-        # substitute all python placeholders with sql placeholders
-        temp_expression = re.sub(r"\{(\d)\}", r"$\1", temp_expression)
-
-        #run the sql query and return it
-        if (row := duckdb.execute(f'SELECT ({temp_expression}) AS "%result%"', [self.environment[x] for x in expression.free_variables]).fetchone()) is not None:
+        if (row := result.fetchone()) is not None:
             return row[0]
         else:
             raise RuntimeError("Embedded SQL expression produced no rows.")
