444584829ba4cae39c6f8fa67d48f289b80e3e38
Tim Fischer <tim.fischer@uni-tuebingen.de>
2024-03-12 13:24:30 +0100

    Add properly parameterised version of ray

--- examples/ray_setup.sql
+++ examples/ray_setup.sql
@@ -1,3 +1,15 @@
+DROP SCHEMA IF EXISTS vec CASCADE;
+CREATE SCHEMA vec;
+CREATE MACRO vec.make(x, y, z) AS (x, y, z) :: struct(x real, y real, z real);
+CREATE MACRO vec.add(a, b) AS vec.make(a.x + b.x, a.y + b.y, a.z + b.z);
+CREATE MACRO vec.sub(a, b) AS vec.make(a.x - b.x, a.y - b.y, a.z - b.z);
+CREATE MACRO vec.mul(v, f) AS vec.make(v.x * f, v.y * f, v.z * f);
+CREATE MACRO vec.div(v, f) AS vec.make(v.x / f, v.y / f, v.z / f);
+CREATE MACRO vec.dot(a, b) AS a.x * b.x + a.y * b.y + a.z * b.z;
+CREATE MACRO vec.cross(a, b) AS vec.make(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
+CREATE MACRO vec.mag(v) AS sqrt(v.x ** 2 + v.y ** 2 + v.z ** 2);
+CREATE MACRO vec.norm(v) AS vec.div(v, vec.mag(v));
+
 DROP TYPE IF EXISTS material CASCADE;
 CREATE TYPE material AS ENUM ('m', 'r', 'l', 'n');
 
@@ -90,3 +102,60 @@ INSERT INTO spheres VALUES
   (1, 'r', NULL, NULL, NULL,  0.4,-0.6, -0.6, 0.40),
   (2, 'm', 0.2 , 0.8 , 0.2 ,  1.0, 0.0,  0.5, 0.25),
   (3, 'l', NULL, NULL, NULL,  0.0, 0.95, 0.0, 0.35);
+
+
+DROP TABLE IF EXISTS "init";
+CREATE TABLE "init" (
+  "index" int not null,
+  x int not null,
+  y int not null,
+  origin_x real not null,
+  origin_y real not null,
+  origin_z real not null,
+  direction_x real not null,
+  direction_y real not null,
+  direction_z real not null
+);
+
+INSERT INTO "init"
+  SELECT  ROW_NUMBER() OVER () AS "index",
+          (x) :: int AS "x",
+          (y) :: int AS "y",
+          (0.0) :: double AS "origin_x",
+          (0.0) :: double AS "origin_y",
+          (-4.5) :: double AS "origin_z",
+          (direction.x) :: double AS "direction_x",
+          (direction.y) :: double AS "direction_y",
+          (direction.z) :: double AS "direction_z"
+  FROM    (SELECT $width         AS width,
+                  $height         AS height,
+                  radians(50) AS fov
+          ) AS "constants"(width,height,fov),
+  LATERAL (SELECT vec.sub(
+                    vec.make(0.0, 0.0,  0.0), -- looking at
+                    vec.make(0.0, 0.0, -4.5)  -- camera position
+                  ) AS rot_z,
+                  vec.cross(
+                    vec.make(0.0, 1.0,  0.0), -- up vector
+                    rot_z
+                  ) AS rot_x,
+                  vec.cross(rot_z, rot_x)  AS rot_y
+          ) AS "camera rotation"(rot_z, rot_x, rot_y),
+  LATERAL (SELECT unnest(range(width ))) AS "xs"("x"),
+  LATERAL (SELECT unnest(range(height))) AS "ys"("y"),
+  LATERAL (SELECT sin((((x + 0.5) / width ) - 0.5) * fov * (width / height))) AS "_x"("offset_x"),
+  LATERAL (SELECT sin((((y + 0.5) / height) - 0.5) * fov                   )) AS "_y"("offset_y"),
+  LATERAL (SELECT vec.add(
+                    vec.add(
+                      vec.mul(
+                        vec.norm(rot_x),
+                        offset_x
+                      ),
+                      vec.mul(
+                        vec.norm(rot_y),
+                        offset_y
+                      )
+                    ),
+                    vec.norm(rot_z)
+                  ) AS "direction"
+          ) AS "transient";
