569861ef295996200e5d20083db77520fca1a95c
Tim Fischer <me@timfi.dev>
2026-05-22 14:44:55 +0200

    Updated pretty printer

--- src/flummi/IR/pretty/pretty.py
+++ src/flummi/IR/pretty/pretty.py
@@ -24,5 +24,57 @@ def pretty(primitive: CFP.Primitive) -> str:
             )
             return this
 
+        case CFP.Fork(variables, expression):
+            this = f"FORK {','.join(variable.identifier for variable in variables)} = "
+            this += utils.indent1(
+                dedent(
+                    expression.source.replace("\\", "\\\\").format(
+                        *(
+                            f"{{{argument.identifier}}}"
+                            for argument in expression.arguments
+                        )
+                    )
+                ),
+                " " * len(this),
+            )
+            return this
+
+        case CFP.Gather(aggregates, keys):
+            this = "GATHER "
+            this += ",\n       ".join(
+                variable.identifier
+                + " = "
+                + utils.indent1(
+                    dedent(
+                        expression.source.replace("\\", "\\\\").format(
+                            *(
+                                f"{{{argument.identifier}}}"
+                                for argument in expression.arguments
+                            )
+                        )
+                    ),
+                    " " * (10 + len(variable.identifier)),
+                )
+                for variable, expression in aggregates.items()
+            )
+            if keys:
+                this += (
+                    f"\nBY {','.join(variable.identifier for variable in keys)}"
+                )
+            return this
+
+        case CFP.IsSynced(variable, label, keys):
+            this = "LET " + variable.identifier + " = ALL "
+            if keys:
+                this += ",".join(variable.identifier for variable in keys) + " "
+            this += "AT " + label.identifier
+            return this
+
+        case CFP.Where(variable, inverted):
+            return f"WHERE {inverted * 'NOT '}{variable.identifier}"
+
+        case CFP.Jump(label):
+            return f"JUMP {label.identifier}"
+
         case _:
             return type(primitive).__name__.upper()
--- src/flummi/IR/pretty/render.py
+++ src/flummi/IR/pretty/render.py
@@ -26,8 +26,16 @@ edge [
 NODE_TEMPLATE = '"{label}" [label="{body}",{style}];'
 
 NODE_STYLES: dict[type[CFP.Primitive], str] = {
+    CFP.Stop: "style = filled, fillcolor = gray3, color = white, fontcolor = white",
+    CFP.Start: "style = filled, fillcolor = cornflowerblue, color = white, fontcolor = white",
     CFP.Assignment: "style = filled, fillcolor = royalblue, color = white, fontcolor = white",
     CFP.Emit: "style = filled, fillcolor = navy, color = white, fontcolor = white",
+    CFP.Where: "style = filled, fillcolor = forestgreen, color = white, fontcolor = white",
+    CFP.Merge: "style = filled, fillcolor = olive, color = white, fontcolor = white",
+    CFP.Jump: "style = filled, fillcolor = darkorange, color = white, fontcolor = white",
+    CFP.Fork: "style = filled, fillcolor = darkviolet, color = white, fontcolor = white",
+    CFP.Gather: "style = filled, fillcolor = maroon4, color = white, fontcolor = white",
+    CFP.IsSynced: "style = filled, fillcolor = indigo, color = white, fontcolor = white",
 }
 
 
@@ -46,8 +54,10 @@ def render[A](graph: CFP.Plan, *, font: str = "monospace") -> str:
                     .replace('<', '\\<')
                     .replace('>', '\\>')
                     .replace('"', '\\"')
-                }\\l|{{{label.identifier}\\l|@{label.location.column}:{
-                    label.location.line
+                }\\l|{{{label.identifier}\\l|@{
+                    f'{label.location.column}:{label.location.line}'
+                    if label.location is not None
+                    else '<unknown>'
                 }}}}}"
             ),
             style=NODE_STYLES.get(type(node), ""),
@@ -61,6 +71,12 @@ def render[A](graph: CFP.Plan, *, font: str = "monospace") -> str:
         f'"{source.identifier}":s -> "{target.identifier}":n;'
         for source, targets in graph.successors_of.items()
         for target in targets
+    )
+
+    edges.extend(
+        f'"{label.identifier}":s -> "{primitive.label.identifier}":n;'
+        for label, primitive in graph.primitives.items()
+        if isinstance(primitive, CFP.Jump)
     )
 
     return PRORGAM_TEMPLATE.format(
