a5b9b0385ddd3cb8e8fec14bff079cff98276bee
Tim Fischer <me@timfi.dev>
2026-06-26 16:02:24 +0200

    Switch to bit_xor for state identification

--- examples/connected-components/output.sql
+++ examples/connected-components/output.sql
@@ -7,73 +7,73 @@ WITH RECURSIVE
             CAST((NULL) AS int) AS "component")
       UNION ALL
     (WITH
-       "start.1"("⚙️", "#️⃣") AS (
-         SELECT NULL AS "⚙️",
-                "🔄"."#️⃣" AS "#️⃣"
+       "start.1"("#️⃣", "⚙️") AS (
+         SELECT "🔄"."#️⃣" AS "#️⃣",
+                NULL AS "⚙️"
          FROM   "🔄"
          WHERE  "🔄"."🏷️" IS NOT DISTINCT FROM 'start.1'
        ),
-       "start.2"("node", "⚙️", "component", "#️⃣") AS (
+       "start.2"("node", "#️⃣", "⚙️", "component") AS (
          SELECT "🔄"."node" AS "node",
+                "🔄"."#️⃣" AS "#️⃣",
                 NULL AS "⚙️",
-                "🔄"."component" AS "component",
-                "🔄"."#️⃣" AS "#️⃣"
+                "🔄"."component" AS "component"
          FROM   "🔄"
          WHERE  "🔄"."🏷️" IS NOT DISTINCT FROM 'start.2'
        ),
-       "fork.1"("node", "⚙️", "#️⃣") AS (
+       "fork.1"("node", "#️⃣", "⚙️") AS (
          SELECT "ℚ"."node" AS "node",
-                "start.1"."⚙️" AS "⚙️",
-                "start.1"."#️⃣" AS "#️⃣"
+                "start.1"."#️⃣" AS "#️⃣",
+                "start.1"."⚙️" AS "⚙️"
          FROM   "start.1",
                 (FROM nodes) AS "ℚ"("node")
        ),
-       "assignment.1"("node", "⚙️", "#️⃣", "component") AS (
+       "assignment.1"("node", "#️⃣", "⚙️", "component") AS (
          SELECT "fork.1"."node" AS "node",
-                "fork.1"."⚙️" AS "⚙️",
                 "fork.1"."#️⃣" AS "#️⃣",
+                "fork.1"."⚙️" AS "⚙️",
                 CAST((("fork.1"."node")) AS int) AS "component"
          FROM   "fork.1"
        ),
-       "merge.1"("node", "⚙️", "#️⃣", "component") AS (
-         (SELECT "assignment.1"."node" AS "node",
-                 "assignment.1"."⚙️" AS "⚙️",
-                 "assignment.1"."#️⃣" AS "#️⃣",
-                 "assignment.1"."component" AS "component"
-          FROM   "assignment.1")
-           UNION ALL
+       "merge.1"("node", "#️⃣", "⚙️", "component") AS (
          (SELECT "start.2"."node" AS "node",
-                 "start.2"."⚙️" AS "⚙️",
                  "start.2"."#️⃣" AS "#️⃣",
+                 "start.2"."⚙️" AS "⚙️",
                  "start.2"."component" AS "component"
           FROM   "start.2")
+           UNION ALL
+         (SELECT "assignment.1"."node" AS "node",
+                 "assignment.1"."#️⃣" AS "#️⃣",
+                 "assignment.1"."⚙️" AS "⚙️",
+                 "assignment.1"."component" AS "component"
+          FROM   "assignment.1")
        ),
-       "assignment.2"("⚙️", "old_components", "#️⃣", "node", "component") AS (
+       "assignment.2"("⚙️", "component", "node", "old_components", "#️⃣") AS (
          SELECT "merge.1"."⚙️" AS "⚙️",
-                CAST((sum(DISTINCT ("merge.1"."component")) OVER ()) AS uhugeint) AS "old_components",
-                "merge.1"."#️⃣" AS "#️⃣",
+                "merge.1"."component" AS "component",
                 "merge.1"."node" AS "node",
-                "merge.1"."component" AS "component"
+                CAST((bit_xor(("merge.1"."component")) OVER ()) AS uhugeint) AS "old_components",
+                "merge.1"."#️⃣" AS "#️⃣"
          FROM   "merge.1"
        ),
-       "fork.2"("⚙️", "old_components", "#️⃣", "node", "component") AS (
-         SELECT "assignment.2"."⚙️" AS "⚙️",
+       "fork.2"("component", "⚙️", "old_components", "node", "#️⃣") AS (
+         SELECT "assignment.2"."component" AS "component",
+                "assignment.2"."⚙️" AS "⚙️",
                 "assignment.2"."old_components" AS "old_components",
-                "assignment.2"."#️⃣" AS "#️⃣",
                 "ℚ"."node" AS "node",
-                "assignment.2"."component" AS "component"
+                "assignment.2"."#️⃣" AS "#️⃣"
          FROM   "assignment.2",
                 LATERAL (SELECT there
                          FROM   edges
                          WHERE  here = ("assignment.2"."node")
                          AND    there > ("assignment.2"."component")) AS "ℚ"("node")
        ),
-       "gather.1"("⚙️", "old_components", "#️⃣", "node", "component") AS (
-         SELECT "fork.2"."⚙️" AS "⚙️",
+       "gather.1"("component", "⚙️", "old_components", "node", "#️⃣") AS (
+         SELECT min(("fork.2"."component")) AS "component",
+                "fork.2"."⚙️" AS "⚙️",
                 "fork.2"."old_components" AS "old_components",
-                "fork.2"."#️⃣" AS "#️⃣",
                 "fork.2"."node" AS "node",
-                min(("fork.2"."component")) AS "component"
+                "fork.2"."#️⃣" AS "#️⃣"
          FROM   "fork.2"
          GROUP  BY "fork.2"."⚙️",
                    "fork.2"."old_components",
@@ -81,59 +81,59 @@ WITH RECURSIVE
                    "fork.2"."#️⃣"
          HAVING COUNT(*) > 0
        ),
-       "assignment.3"("⚙️", "old_components", "node", "new_components", "component", "#️⃣") AS (
-         SELECT "gather.1"."⚙️" AS "⚙️",
+       "assignment.3"("component", "new_components", "⚙️", "old_components", "node", "#️⃣") AS (
+         SELECT "gather.1"."component" AS "component",
+                CAST((bit_xor(("gather.1"."component")) OVER ()) AS uhugeint) AS "new_components",
+                "gather.1"."⚙️" AS "⚙️",
                 "gather.1"."old_components" AS "old_components",
                 "gather.1"."node" AS "node",
-                CAST((sum(DISTINCT ("gather.1"."component")) OVER ()) AS uhugeint) AS "new_components",
-                "gather.1"."component" AS "component",
                 "gather.1"."#️⃣" AS "#️⃣"
          FROM   "gather.1"
        ),
-       "assignment.4"("⚙️", "#️⃣", "node", "cond", "component") AS (
+       "assignment.4"("⚙️", "component", "node", "cond", "#️⃣") AS (
          SELECT "assignment.3"."⚙️" AS "⚙️",
-                "assignment.3"."#️⃣" AS "#️⃣",
+                "assignment.3"."component" AS "component",
                 "assignment.3"."node" AS "node",
                 CAST((("assignment.3"."old_components") <> ("assignment.3"."new_components")) AS boolean) AS "cond",
-                "assignment.3"."component" AS "component"
+                "assignment.3"."#️⃣" AS "#️⃣"
          FROM   "assignment.3"
        ),
-       "where.1"("node", "component", "#️⃣") AS (
+       "where.1"("node", "#️⃣", "component") AS (
          SELECT "assignment.4"."node" AS "node",
-                "assignment.4"."component" AS "component",
-                "assignment.4"."#️⃣" AS "#️⃣"
+                "assignment.4"."#️⃣" AS "#️⃣",
+                "assignment.4"."component" AS "component"
          FROM   "assignment.4"
          WHERE  "assignment.4"."cond" IS NOT DISTINCT FROM TRUE
        ),
-       "where.2"("node", "⚙️", "#️⃣", "component") AS (
+       "where.2"("node", "#️⃣", "⚙️", "component") AS (
          SELECT "assignment.4"."node" AS "node",
-                "assignment.4"."⚙️" AS "⚙️",
                 "assignment.4"."#️⃣" AS "#️⃣",
+                "assignment.4"."⚙️" AS "⚙️",
                 "assignment.4"."component" AS "component"
          FROM   "assignment.4"
          WHERE  "assignment.4"."cond" IS DISTINCT FROM TRUE
        ),
-       "gather.2"("⚙️", "nodes", "#️⃣") AS (
-         SELECT "where.2"."⚙️" AS "⚙️",
-                list(("where.2"."node")) AS "nodes",
-                "where.2"."#️⃣" AS "#️⃣"
+       "gather.2"("nodes", "#️⃣", "⚙️") AS (
+         SELECT list(("where.2"."node")) AS "nodes",
+                "where.2"."#️⃣" AS "#️⃣",
+                "where.2"."⚙️" AS "⚙️"
          FROM   "where.2"
-         GROUP  BY "where.2"."⚙️",
-                   "where.2"."#️⃣",
+         GROUP  BY "where.2"."#️⃣",
+                   "where.2"."⚙️",
                    "where.2"."component"
          HAVING COUNT(*) > 0
        ),
-       "jump.1"("🏷️", "node", "component", "#️⃣") AS (
+       "jump.1"("🏷️", "component", "#️⃣", "node") AS (
          SELECT 'start.2' AS "🏷️",
-                "where.1"."node" AS "node",
                 "where.1"."component" AS "component",
-                "where.1"."#️⃣" AS "#️⃣"
+                "where.1"."#️⃣" AS "#️⃣",
+                "where.1"."node" AS "node"
          FROM   "where.1"
        ),
-       "emit.1"("⚙️", "#️⃣", "📊") AS (
-         SELECT "gather.2"."⚙️" AS "⚙️",
+       "emit.1"("📊", "#️⃣", "⚙️") AS (
+         SELECT "gather.2"."nodes" AS "📊",
                 "gather.2"."#️⃣" AS "#️⃣",
-                "gather.2"."nodes" AS "📊"
+                "gather.2"."⚙️" AS "⚙️"
          FROM   "gather.2"
        ),
        "stop.1"("⚙️") AS (
--- examples/connected-components/setup.sql
+++ examples/connected-components/setup.sql
@@ -13,14 +13,14 @@ CREATE TABLE edges(
 
 INSERT INTO nodes
 SELECT x - 1 AS id
-FROM   generate_series(1, 10_000_00) AS _(x);
+FROM   generate_series(1, 100_000) AS _(x);
 
 -- connect ~10% of node pairs with a directed edge from the smaller to the higher node id
 INSERT INTO edges
 SELECT here.id, there.id
 FROM   nodes AS here,
        nodes AS there
-WHERE  random() < 0.1
+WHERE  random() < 0.15
 AND    here.id < there.id
 AND    here.id % 1000 = there.id % 1000;
 
