a688b28ec853b304cfd25adf58713f3a6f733908
Torsten Grust <torsten.grust@gmail.com>
2026-08-30 16:24:57 -0400

    CC: add UNION variant of USING KEY query (only DuckDB v2.0)

--- examples/connected-components/using-key.sql
+++ examples/connected-components/using-key.sql
@@ -10,3 +10,18 @@ WITH RECURSIVE cc(node, comp) USING KEY (node, min(comp)) AS (
 SELECT list(node)
 FROM   cc
 GROUP  BY comp;
+
+
+-- ⚠️ Only DuckDB v2.0 supports USING KEY with UNION semantics
+--     (stop once the component assigned to nodes does not change)
+WITH RECURSIVE cc(node, comp) USING KEY (node, min(comp)) AS (
+  SELECT id AS node, id AS comp
+  FROM   nodes
+    UNION
+  SELECT u.node, v.comp
+  FROM   recurring.cc AS u, edges AS e, cc AS v
+  WHERE  (e.here, e.there) = (u.node, v.node)
+)
+SELECT list(node)
+FROM   cc
+GROUP  BY comp;
